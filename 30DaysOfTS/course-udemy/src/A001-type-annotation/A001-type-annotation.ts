58f857f0874ddffbc370967429325accf7136036
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-06-19 15:31:58 -0300

    couse about typescript udemy, checking the types

--- 30DaysOfTS/course-udemy/src/A001-type-annotation/A001-type-annotation.ts
+++ 30DaysOfTS/course-udemy/src/A001-type-annotation/A001-type-annotation.ts
@@ -5,3 +5,21 @@ let adulto: boolean = true // true ou false
 let simbolo: symbol = Symbol('simbolo') // symbol
 // let big = bigint = 10n // bigint
 
+
+// Arrays
+// Outra maneira de declarar um array: let arrayDeNumeros: number[] = [1, 2, 3, 4, 5] // Array
+let arrayDeNumeros: Array<number> = [1, 2, 3, 4, 5] // Array<number> = [1, 2, 3, 4, 5]
+let arrayDeStrings: Array<string> = ['João', 'Maria', 'Pedro'] // Array<string> = ['João', 'Maria', 'Pedro']
+
+// Objects
+// Quando tem ? no final do tipo, é opcional
+let pessoa: { nome: string, idade: number, adulto?: boolean } = { nome: 'João', idade: 20 } // Objects
+
+// function
+
+function soma(a: number, b: number): number {
+    return a + b
+}
+
+// Colocando funções na variável e com os tipos, ainda sem o alias
+let soma2: (a: number, b: number) => number = (a, b) => a + b
