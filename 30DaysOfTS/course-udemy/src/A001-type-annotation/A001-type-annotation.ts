78229b874f4ac676bccd45bdbb7c16e50791440a
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-06-19 15:11:28 -0300

    little changes for test message using git from vscode

---
+++ 30DaysOfTS/course-udemy/src/A001-type-annotation/A001-type-annotation.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+let nome: string = 'João' // qualquer tipo de strings: '' ou "", `${}`
+let idade: number = 20 // 1, 1.234, 1.234.567, -5.55, -0xf00d
+let adulto: boolean = true // true ou false
+let simbolo: symbol = Symbol('simbolo') // symbol
+// let big = bigint = 10n // bigint
+
