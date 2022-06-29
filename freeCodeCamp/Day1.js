a32633efc518d1b9c56a9a067c5c3eac370b9568
Valdeni <valdenidelgadofilho@gmail.com>
2022-06-28 22:17:03 -0300

    I am start freeCodeCamp for study JS too

---
+++ freeCodeCamp/Day1.js
@@ -0,0 +1,80 @@
+// 1 - Comments
+// This is an in-line comment.
+
+/* This is a 
+multi-line comment */
+
+// 2 - Variable
+// Use the var keyword to create a variable called myName.
+var myName;
+
+
+// 3 - Variable Scope
+/*
+First, this code creates a variable named myVar. Then, the code assigns 5 to myVar. 
+Now, if myVar appears again in the code, the program will treat it as if it is 5.
+*/
+// Setup
+var a;
+// Only change code below this line
+var a = 7;
+
+// 4 - Assigning the Value of One Variable to Another
+// Assign the contents of a to variable b.
+// Setup
+var a;
+a = 7;
+var b;
+// Only change code below this line
+b = a;
+
+// Define a variable a with var and initialize it to a value of 9.
+
+var a = 9;
+
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+var myFirstName = 'Valdeni';
+var myLastName = 'Delgado';
+
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/*Modify the existing declarations and assignments so their names use camelCase.
+Do not create any new variables.*/
+// Variable declarations
+// var StUdLyCapVaR;
+// var properCamelCase;
+// var TitleCaseOver;
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Update the code so it only uses the let keyword.
+let catName = "Oliver";
+let catSound = "Meow!";
+
+/*
+Change the code so that all variables are declared using let or const. 
+Use let when you want the variable to change, and const when you want the variable to remain constant. 
+Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+*/
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// Change the 0 so that sum will equal 20.
+const sum = 10 + 10;
