980db8b47814f96ab50e9ef2f8e8a5f7277e200c
Valdeni <valdenidelgadofilho@gmail.com>
2022-07-04 20:53:53 -0300

    Create Category and Article model

---
+++ NodeJS/GuiaPress/categories/Category.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/db');
+
+const Category = connection.define('categories', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+
+module.exports = Category;
