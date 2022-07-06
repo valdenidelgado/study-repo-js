b4847ec77b86cbe1a1c0a1b5bdc80779be3cb99c
Valdeni <valdenidelgadofilho@gmail.com>
2022-07-05 23:35:27 -0300

    Add navbar and form category, add routes post and get too

--- NodeJS/GuiaPress/categories/CategoriesControllers.js
+++ NodeJS/GuiaPress/categories/CategoriesControllers.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
+
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+    let title = req.body.title;
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/')
+        });
+    }else{
+        res.redirect('/admin/categories/new');
+    }
+});
 
 router.get('/categories', (req, res) => {
     res.send('ROTA CATEGORIAS');
-});
-
-router.get('/admin/categories/new', (req, res) => {
-   res.send('ROTA PARA CRIAR UMA NOVA CATEGORIA');
 });
 
 module.exports = router;
