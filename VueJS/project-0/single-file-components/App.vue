45c969b2c7f6b44650927c328626dee63a7a938a
Valdeni <valdenidelgadofilho@gmail.com>
2022-07-28 19:37:30 -0300

    vue and html/css files

---
+++ VueJS/project-0/single-file-components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h1>Hello World</h1>
+        <p>This is a simple example of a Vue.js component.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
