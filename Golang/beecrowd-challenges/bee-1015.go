1e58c342bb23f3f200c3809576a67449c4b44b48
Valdeni <valdenidelgadofilho@gmail.com>
2022-08-05 22:30:27 -0300

    beecrowd 1015

--- Golang/beecrowd-challenges/bee-1015.go
+++ Golang/beecrowd-challenges/bee-1015.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	name := "Valdeni"
-	fmt.Printf("%s", name)
+	var x float64
+	fmt.Scan(&x)
+	var y float64
+	fmt.Scan(&y)
+	var x2 float64
+	fmt.Scan(&x2)
+	var y2 float64
+	fmt.Scan(&y2)
+
+	calc := math.Sqrt(math.Pow(x2-x, 2) + math.Pow(y2-y, 2))
+
+	fmt.Printf("%.4f", calc)
 }
