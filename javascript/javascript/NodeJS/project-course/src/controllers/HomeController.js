0ff070bbfebbde0fa9e1245eca1f4c3933761e40
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-07 03:18:45 -0300

    reoganize folders and files

--- javascript/javascript/NodeJS/project-course/src/controllers/HomeController.js
+++
@@ -1,7 +0,0 @@
-class HomeController {
-  async index(req, res) {
-    return res.json({ message: 'Hello World' });
-  }
-}
-
-export default new HomeController();
---
+++ typescript/30DaysOfTS/configTs/src/server.ts
@@ -0,0 +1,8 @@
+const name: string = 'Valdeni'
+let lastName: string = "Delgado"
+
+function sayHello(name: string, lastName: string): void {
+    console.log(`Hello ${name} ${lastName}`)
+}
+
+sayHello(name, lastName)
