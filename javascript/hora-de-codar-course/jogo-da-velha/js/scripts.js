3a853b4a57fe682cb5bcd210caa19f2402397863
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-07 04:34:07 -0300

    wip: logic js

---
+++ javascript/hora-de-codar-course/jogo-da-velha/js/scripts.js
@@ -0,0 +1,57 @@
+let x = document.querySelector('.x')
+let o = document.querySelector('.o')
+let boxes = document.querySelectorAll('.box')
+let buttons = document.querySelectorAll('#buttons-container button')
+let messageContainer = document.querySelector('#message')
+let messageText = document.querySelector('#message p')
+let secondPlayer
+
+// contador de jogadas
+let player1 = 0
+let player2 = 0
+
+// adicionando o evento de click aos boxes
+
+for (let i = 0; i < boxes.length; i++) {
+
+  // quando alguem clica na caixa
+  boxes[i].addEventListener('click', function () {
+
+    let el = checkEl(player1, player2)
+
+    // verifica se ja tem x ou o
+    if(this.childNodes.length == 0) {
+      let cloneEl = el.cloneNode(true)
+
+      this.appendChild(cloneEl)
+
+      // computar jogada
+      if(player1 == player2) {
+        player1++
+
+        if(secondPlayer == 'ai-player') {
+          computerPlay()
+          player2++
+        }
+      } else {
+        player2++
+      }
+
+      // checa quem venceu
+      checkWinCondition()
+    }
+
+  })
+}
+
+function checkEl(player1, player2) {
+  if(player1 == player2) {
+    // x
+    el = x
+  } else {
+    // o
+    el = o
+  }
+
+  return el
+}
