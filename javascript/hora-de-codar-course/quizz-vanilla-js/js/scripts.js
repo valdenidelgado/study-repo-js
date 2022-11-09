37fc73dd0a86d57e76658d916526488e568a1408
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-09 18:17:14 -0300

    finished project

--- javascript/hora-de-codar-course/quizz-vanilla-js/js/scripts.js
+++ javascript/hora-de-codar-course/quizz-vanilla-js/js/scripts.js
@@ -1,97 +1,213 @@
-// variables
-const question = document.querySelector('#question')
-const answersBox = document.querySelector('#answers-box')
-const quizzContainer = document.querySelector('#quizz-container')
-const scoreContainer = document.querySelector('#score-container')
-const letters = ['a', 'b', 'c', 'd']
+// Declarações variáveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
 
-let points = 0
-let actualQuestion = 0
-
-// Questions
-
+// Perguntas
 const questions = [
   {
     "question": "PHP foi desenvolvido para qual fim?",
     "answers": [
       {
-        "answer": "Desenvolvimento de sites",
+        "answer": "back-end",
         "correct": true
       },
       {
-        "answer": "Desenvolvimento de jogos",
+        "answer": "front-end",
         "correct": false
       },
       {
-        "answer": "Desenvolvimento de aplicativos",
+        "answer": "Sistema operacional",
         "correct": false
       },
       {
-        "answer": "Desenvolvimento de sistemas operacionais",
+        "answer": "Banco de dados",
         "correct": false
-      }
+      },
     ]
   },
   {
-    "question": "Uma forma de declarar uma variavel em JavaScript é:",
+    "question": "Uma forma de declarar variável em JavaScript:",
     "answers": [
       {
-        "answer": "var nomeDaVariavel = 'valor'",
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
         "correct": true
       },
       {
-        "answer": "nomeDaVariavel = 'valor'",
+        "answer": "@var",
         "correct": false
       },
       {
-        "answer": "nomeDaVariavel: 'valor'",
+        "answer": "#let",
         "correct": false
       },
-      {
-        "answer": "nomeDaVariavel = valor",
-        "correct": false
-      }
     ]
   },
   {
-    "question": "Qual a sintaxe correta para se criar um elemento em JavaScript?",
+    "question": "Qual o seletor de id no CSS?",
     "answers": [
       {
-        "answer": "document.createElement('p')",
+        "answer": "#",
         "correct": true
       },
       {
-        "answer": "document.createClass('p')",
+        "answer": ".",
         "correct": false
       },
       {
-        "answer": "document.createElement('#p')",
+        "answer": "@",
         "correct": false
       },
       {
-        "answer": "document.createElement.id('p')",
+        "answer": "/",
         "correct": false
-      }
+      },
     ]
   },
-]
+];
 
-function init() {
-  createQuestion(actualQuestion)
-}
+// Substituição do quizz para a primeira pergunta 
+function init () {
+  // criar a primeira pergunta
+  createQuestion(0);
+};
 
+// Cria uma pergunta
 function createQuestion(i) {
-  const oldButtons = document.querySelectorAll('button')
+  // Limpar a questão anterior
+  const oldButtons = answersBox.querySelectorAll("button");
 
-  oldButtons.forEach(button => {
-    button.remove()
-  })
+  oldButtons.forEach(btn => {
+    btn.remove();
+  });
 
-  const questionText = question.querySelector('#question-text')
-  const questionNumber = question.querySelector('#question-number')
+  // Alterar o texto da pergunta
+  const questionText = question.querySelector("#question-text");
+  const questionNumber = question.querySelector("#question-number");
 
-  questionText.textContent = questions[i].question
-  questionNumber.textContent = i + 1
+  questionText.textContent = questions[i].question;
+  questionNumber.textContent = i + 1;
+
+  // Insere as alternativas
+  questions[i].answers.forEach((answer, i) => {
+    // Cria o template do botão do quizz
+    const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+
+    const letterBtn = answerTemplate.querySelector(".btn-letter");
+    const answerText = answerTemplate.querySelector(".question-answer");
+
+    // Seleciona a letra da alternativa
+    letterBtn.textContent = letters[i];
+    // Seleciona o texto da alternativa
+    answerText.textContent = answer['answer'];
+
+    answerTemplate.setAttribute("correct-answer", answer['correct']);
+
+    // Remover hide e template class
+    answerTemplate.classList.remove("hide");
+    answerTemplate.classList.remove("answer-template");
+
+    // Inserir a alternativa na tela
+    answersBox.appendChild(answerTemplate);
+
+    // Inserir um evento do clique no botão
+    answerTemplate.addEventListener("click", function() {
+      checkAnswer(this);
+    })
+  });
+
+  // Incrementar o número da questão
+  actualQuestion++;
 }
 
-init()
+// Verificando resposta do usuário
+function checkAnswer(btn) {
+  // Selecionando todos os botões
+  const buttons = answersBox.querySelectorAll("button");
+
+  // Verifica se a resposta está correta e adiciona classes nos botões
+  buttons.forEach(function(button) {
+    if(button.getAttribute("correct-answer") === "true") {
+
+      button.classList.add("correct-answer");
+
+      // checa se o usuário acertou a pergunta
+      if(btn === button) {
+        // incremento dos pontos
+        points++;
+      }
+
+    } else {
+
+      button.classList.add("wrong-answer");
+
+    }
+  });
+
+  // Exibir próxima pergunta
+  nextQuestion();
+};
+
+// Exibe a próxima pergunta do Quizz
+function nextQuestion() {
+
+  // timer para o usúario ver a resposta
+  setTimeout(function() {
+    // verifica se ainda existe perguntas
+    if(actualQuestion >= questions.length) {
+      // apresentar a mensagem de sucesso
+      showSuccessMessage();
+      return;
+    }
+
+    createQuestion(actualQuestion);
+  },1500)
+};
+
+// Exibe a tela final
+function showSuccessMessage() {
+  hideOrShowQuizz();
+
+  //calcular o score
+  const score = ((points / questions.length) * 100).toFixed(2);
+
+  // trocar dados da tela de sucesso
+  const displayScore = document.querySelector('#display-score span');
+  displayScore.textContent = score.toString();
+
+  // Alterar o número de perguntas corretas
+  const correctAnswers = document.querySelector("#correct-answers");
+  correctAnswers.textContent = points;
+
+  // Altera a quantidade de perguntas
+  const totalQuestions = document.querySelector("#questions-qty");
+  totalQuestions.textContent = questions.length;
+};
+
+// Mostra ou esconde o score
+function hideOrShowQuizz() {
+  quizzContainer.classList.toggle("hide");
+  scoreContainer.classList.toggle("hide");
+};
+
+// Reiniciar Quizz
+const restartBtn = document.querySelector('#restart')
+restartBtn.addEventListener("click", function() {
+  // zerar o jogo
+  actualQuestion = 0;
+  points = 0;
+
+  hideOrShowQuizz();
+  init();
+});
+ 
+// Inicialização do Quizz
+init();
