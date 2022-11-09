3907ea681e85b3991b17428f7f3e89a76898871b
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-09 19:23:11 -0300

    feat: connected sqlite bd

--- javascript/hora-de-codar-course/node-with-front/node-express-sequelize/app.js
+++ javascript/hora-de-codar-course/node-with-front/node-express-sequelize/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connection')
 
 const PORT = 3000
 
@@ -7,6 +8,14 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
 
+// db connection
+db.authenticate().then(() => {
+  console.log('Database connected')
+  }).catch(err => {
+    console.log('Error: ' + err)
+  })
+
+// routes
 app.get('/', (req, res) => {
   res.send('Hello World 2')
 })
