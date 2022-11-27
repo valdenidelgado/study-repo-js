17f01038c5c83a9f05325cbeb8a91a75bb268163
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-27 16:47:34 -0300

    data types

---
+++ javascript/hora-de-codar-course/exercices/tipoDeDados/exer01.js
@@ -0,0 +1,7 @@
+const string1 = 'Hello'
+const string2 = 'Hello Again'
+const string3 = 'Hello with template literals'
+
+console.log("example 1: " + string1)
+console.log('example 2: ' + string2)
+console.log(`example 3: ${string3}`)
---
+++ javascript/hora-de-codar-course/exercices/tipoDeDados/exer04.js
@@ -0,0 +1,3 @@
+console.log(1 == 2 && 2 == 1)
+console.log(1 > 2 || 2 == 0)
+console.log(!2 == 2)
