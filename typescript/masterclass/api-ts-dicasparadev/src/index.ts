cdcb4e7feee97446f26a2bf1927c6e8c5d2c9524
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-12-15 15:54:43 -0300

    feat: add express initial server

--- typescript/masterclass/api-ts-dicasparadev/src/index.ts
+++ typescript/masterclass/api-ts-dicasparadev/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send('Hello World! Mundão doido')
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
