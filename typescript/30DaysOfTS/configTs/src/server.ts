a815481d0a8525a3cb481983aed3a1e1ee312a25
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-12-16 12:30:28 -0300

    some things

--- typescript/30DaysOfTS/configTs/src/server.ts
+++ typescript/30DaysOfTS/configTs/src/server.ts
@@ -5,4 +5,12 @@ function sayHello(name: string, lastName: string): void {
     console.log(`Hello ${name} ${lastName}`)
 }
 
+function sum(a:number, b:number): number {
+    return a + b
+}
+
 sayHello(name, lastName)
+
+
+console.log(`A soma de 2 + 2 é: ${sum(2,2)}`);
+
