e5b9b86d52b8f223a15a8ad2b810932823a4bdae
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-12-17 01:41:07 -0300

    refactor: add HttpStatusCode Enum

--- typescript/masterclass/api-ts-dicasparadev/src/controllers/helpers.ts
+++ typescript/masterclass/api-ts-dicasparadev/src/controllers/helpers.ts
@@ -1,25 +1,25 @@
-import { HttpResponse } from "./protocols";
+import { HttpResponse, HttpStatusCode } from "./protocols";
 
 export const created = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.CREATED,
   body,
 });
 
 export const ok = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.OK,
   body,
 });
 
 export const badRequest = (message: string): HttpResponse<string> => {
   return {
-    statusCode: 400,
+    statusCode: HttpStatusCode.BAD_REQUEST,
     body: message,
   };
 };
 
 export const serverError = (): HttpResponse<string> => {
   return {
-    statusCode: 400,
+    statusCode: HttpStatusCode.SERVER_ERROR,
     body: "Internal server error!",
   };
 };
