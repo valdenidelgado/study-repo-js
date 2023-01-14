61cd420e4eed78b962a8e8fc3b0feb9ace008840
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-01-14 11:13:05 -0300

    fix

--- javascript/hora-de-codar-course/api-rest-mvc-mongo/backend/db/conn.js
+++ javascript/hora-de-codar-course/api-rest-mvc-mongo/backend/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 async function main() {
-  await mongoose.connect("mongodb://root:root@localhost:27017/getapet");
+  await mongoose.connect("mongodb://localhost:27017/getapet");
   console.log("Connected to mongoose");
 }
 
