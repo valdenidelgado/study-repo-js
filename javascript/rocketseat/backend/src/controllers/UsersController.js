b687b9af40a0c041d2ed87310f05ddac16ded16c
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-01-21 18:27:10 -0300

    config errors handler

--- javascript/rocketseat/backend/src/controllers/UsersController.js
+++ javascript/rocketseat/backend/src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
-  async createUser(req, res) {
+  createUser(req, res) {
     const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      throw new AppError("Missing name, email or password", 400);
+    }
+
     res.status(201).json({ name, email, password });
   }
 }
--- javascript/rocketseat/backend/src/server.js
+++ javascript/rocketseat/backend/src/server.js
@@ -1,3 +1,5 @@
+require("express-async-errors");
+const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
 const app = express();
@@ -5,6 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((err, req, res, next) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
