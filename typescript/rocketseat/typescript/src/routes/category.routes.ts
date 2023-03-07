9fa647384622bceff0b399f258d36732b0d4a3e6
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-03-07 09:14:56 -0300

    some changes and add docker

--- typescript/rocketseat/typescript/src/routes/category.routes.ts
+++ typescript/rocketseat/typescript/src/routes/category.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 import { listCategoryController } from "../modules/cars/useCases/listCategory";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -10,6 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
   return listCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
--- typescript/rocketseat/typescript/src/server.ts
+++ typescript/rocketseat/typescript/src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
 
 app.listen(3000, () => {
   console.log(`Server listening on port ${3000}`);
