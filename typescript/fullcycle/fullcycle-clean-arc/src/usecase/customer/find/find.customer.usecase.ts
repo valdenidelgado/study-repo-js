0a061b5968e3722e4e26f45539cba6913d3246be
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-06-08 13:40:34 -0300

    create usecase to find customer

---
+++ typescript/fullcycle/fullcycle-clean-arc/src/usecase/customer/find/find.customer.usecase.ts
@@ -0,0 +1,22 @@
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
+import { InputFindCustomerDTO, OutputFindCustomerDTO } from "./find.customer.dto";
+
+export default class FindCustomerUseCase {
+  private customerRepository: CustomerRepositoryInterface;
+  constructor(customerRepository: CustomerRepositoryInterface) {
+      this.customerRepository = customerRepository;
+  }
+  async execute(input: InputFindCustomerDTO): Promise<OutputFindCustomerDTO> {
+      const customer = await this.customerRepository.find(input.id);
+      return {
+          id: customer.id,
+          name: customer.name,
+          address: {
+              street: customer.address.street,
+              number: customer.address.number,
+              city: customer.address.city,
+              zip: customer.address.zip,
+          },
+      };
+  }
+}
