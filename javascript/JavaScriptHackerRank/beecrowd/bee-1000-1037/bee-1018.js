fda07053c5e8caafb91ab1c9f0f0abec2ac3d7af
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-23 10:45:49 -0300

    resolve bee1018

--- javascript/JavaScriptHackerRank/beecrowd/bee-1000-1037/bee-1018.js
+++ javascript/JavaScriptHackerRank/beecrowd/bee-1000-1037/bee-1018.js
@@ -9,17 +9,17 @@ let cedulas = parseInt(lines.shift())
 console.log(cedulas)
 
 
-let aux = cedulas / 100
-console.log(`${aux} nota(s) de R$ 100,00`)
-aux = cedulas % 100
+let aux = cedulas
+console.log(`${parseInt(aux / 100)} nota(s) de R$ 100,00`)
+aux %= 100
 console.log(`${parseInt(aux / 50)} nota(s) de R$ 50,00`)
-aux = cedulas % 50
+aux %= 50
 console.log(`${parseInt(aux / 20)} nota(s) de R$ 20,00`)
-aux = cedulas % 20
+aux %= 20
 console.log(`${parseInt(aux / 10)} nota(s) de R$ 10,00`)
-aux = cedulas % 10
+aux %= 10
 console.log(`${parseInt(aux / 5)} nota(s) de R$ 5,00`)
-aux = cedulas % 5
+aux %= 5
 console.log(`${parseInt(aux / 2)} nota(s) de R$ 2,00`)
-aux = cedulas % 2
+aux %= 2
 console.log(`${parseInt(aux / 1)} nota(s) de R$ 1,00`)
