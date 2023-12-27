473ac6264d225dcdf993b7bc634d4c813e1302d7
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-12-27 16:13:55 -0300

    transaction table

--- react/project-rocket2/03-dt-money/src/pages/transactions/index.tsx
+++ react/project-rocket2/03-dt-money/src/pages/transactions/index.tsx
@@ -1,11 +1,47 @@
-import {Header} from "../../components/Header";
-import {Summary} from "../../components/Summary";
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
-      <Header/>
-      <Summary/>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 2.000,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Ar condicionado</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 500,00</PriceHighlight>
+              </td>
+              <td>Casa</td>
+              <td>1/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
-  )
+  );
 }
